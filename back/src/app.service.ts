fbf4f0fb42b053e00b9a01d586c699d164097753
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2023-02-09 00:21:51 +0700

    # changed app config

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World! I'm Eugene Bystrov from 8K93 of TPU!";
   }
 }

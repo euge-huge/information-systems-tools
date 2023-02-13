0dc2d3590ff7d27a007445bb04fcd2c2ff94de58
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2023-02-14 00:10:37 +0700

    # refactored app

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -3,10 +3,15 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): Promise<string> {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve("Hello World! I'm Eugene Bystrov from 8K93 of TPU!");
-      }, 36000);
+        const oneOrTwo = Math.floor(Math.random() * 2);
+
+        if (oneOrTwo === 1)
+          resolve("Hello World! I'm Eugene Bystrov from 8K93 of TPU!");
+
+        reject('Rejected');
+      }, 10);
     });
   }
 }

a8877b72495ce356994cd6553bd98f3c757b92c9
Eugene Bystrov <evgeniy0ctreles@gmail.com>
2023-02-13 23:59:41 +0700

    # added kuber yaml and refactored app

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World! I'm Eugene Bystrov from 8K93 of TPU!";
+  getHello(): Promise<string> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve("Hello World! I'm Eugene Bystrov from 8K93 of TPU!");
+      }, 36000);
+    });
   }
 }
